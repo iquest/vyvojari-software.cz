696d7300bd4bebc25e5ea345616025a3acb38f1d
Maksym Shcherban <peregrin@ukr.net>
2018-01-18 18:00:18 +0100

    v1

---
+++ scripts/main.js
@@ -0,0 +1,73 @@
+$(function(){
+	//Scrollable links
+	$('a.scroll-link').click(function(){
+		//var offset = -$('#main-nav').height();
+		var offset = parseInt($(this).attr('data-scroll-offset'));
+		if(isNaN(offset)){
+			offset = -50;
+		}
+		$.scrollTo($(this).attr('href'), {
+			duration : 500,
+			offset: offset
+		});
+		return false;
+	});
+
+    //Slider
+    $('.bxslider').bxSlider({
+		auto: true,
+		pager: false,
+		controls: true,
+		touchEnabled: true
+    });
+
+	//Scroll animation
+	AOS.init({
+		offset: 50,
+		duration: 800,
+	});
+
+	//Misc
+	$('[data-copy-to]').each(function(){
+		$($(this).attr('data-copy-to')).html($(this).html());
+	});
+
+    //Roles nav
+    $('#role-1').addClass('active');
+    $('#role-1 .role-description').fadeIn();
+    $('#role-1 .role-description-in').slideDown();
+
+    function roleDeactivate(){
+		var active = $('.role-item.active');
+		if(active.length){
+			active.find('.role-description').slideUp();
+			active.find('.role-description-in').fadeOut();
+			active.removeClass('active');
+		}
+    }
+
+    function roleActivate(p){
+		if(p.length){
+			p.find('.role-description').slideDown();
+			p.find('.role-description-in').fadeIn();
+			p.addClass('active');
+		}
+    }
+
+    $('.role-person-link').click(function(){
+    	p = $(this).parents('.role-item');
+		if(!$(this).hasClass('.role-dodavatel') && !p.hasClass('active')){
+			roleDeactivate();
+			roleActivate(p);
+		}
+    });
+
+    $('.btn-next-role').click(function(){
+		roleDeactivate();
+		roleActivate($($(this).attr('data-target')));
+    });
+
+	function bsIs(env){
+		return $('#environment-' + env).is(':visible');
+	}
+});
